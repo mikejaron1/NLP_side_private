c267a5dd23c540323f965dfcc240194ba6a78564
Meredith Meeks <meredith.meeks@columbia.edu>
2016-11-18 15:13:59 -0500

    updated

--- Text_Clustering.py
+++ Text_Clustering.py
@@ -1,1 +1,12 @@
-
+import numpy as np
+import pandas as pd
+import nltk
+import re
+import os
+import codecs
+from sklearn import feature_extraction
+import mpld3
+# nltk.download()
+stopwords = nltk.corpus.stopwords.words('english')
+print stopwords[:]
+print len(stopwords), "total stopwords"
