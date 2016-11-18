e1bb89b5a9496c150d487d258dd8176f69192e73
Mike <mikejaron@gmail.com>
2016-11-18 16:24:38 -0500

    started text clustering

--- Text_Clustering.py
+++ Text_Clustering.py
@@ -5,10 +5,91 @@ import re
 import os
 import codecs
 from sklearn import feature_extraction
-import mpld3
+# import mpld3b
+from nltk.stem.snowball import SnowballStemmer
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
 # nltk.download()
 stopwords = nltk.corpus.stopwords.words('english')
-# print stopwords[:]
-# print len(stopwords), "total stopwords"
-from nltk.stem.snowball import SnowballStemmer
+
 stemmer = SnowballStemmer("english")
+
+## get in our documents
+directory = './'
+cnn_articles = directory+'articles/'
+fox_articles = directory+'fox_articles/'
+
+for i in os.listdir(fox_articles):
+	if '.txt' in i:
+		text = open(fox_articles+i).read().split('\n')
+		# print text
+		break
+
+
+# here I define a tokenizer and stemmer which returns the set of stems in the text that it is passed
+def tokenize_and_stem(text):
+    # first tokenize by sentence, then by word to ensure that punctuation is caught as it's own token
+    tokens = [word for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
+    filtered_tokens = []
+    # filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
+    for token in tokens:
+        if re.search('[a-zA-Z]', token):
+            filtered_tokens.append(token)
+    stems = [stemmer.stem(t) for t in filtered_tokens]
+    return stems
+
+
+def tokenize_only(text):
+    # first tokenize by sentence, then by word to ensure that punctuation is caught as it's own token
+    tokens = [word.lower() for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
+    filtered_tokens = []
+    # filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
+    for token in tokens:
+        if re.search('[a-zA-Z]', token):
+            filtered_tokens.append(token)
+    return filtered_tokens
+
+
+#not super pythonic, no, not at all.
+#use extend so it's a big flat list of vocab
+totalvocab_stemmed = []
+totalvocab_tokenized = []
+for i in text:
+	allwords_stemmed = tokenize_and_stem(i) #for each item in 'synopses', tokenize/stem
+	totalvocab_stemmed.extend(allwords_stemmed) #extend the 'totalvocab_stemmed' list
+
+	allwords_tokenized = tokenize_only(i)
+	totalvocab_tokenized.extend(allwords_tokenized)
+
+# print totalvocab_stemmed
+# print totalvocab_tokenized
+
+
+vocab_frame = pd.DataFrame({'words': totalvocab_tokenized}, index = totalvocab_stemmed)
+print 'there are ' + str(vocab_frame.shape[0]) + ' items in vocab_frame'
+
+
+#define vectorizer parameters
+tfidf_vectorizer = TfidfVectorizer(max_df=0.8, max_features=200000,
+                                 min_df=0.1, stop_words='english',
+                                 use_idf=True, tokenizer=tokenize_and_stem, ngram_range=(1,3))
+
+tfidf_matrix = tfidf_vectorizer.fit_transform(text) #fit the vectorizer to synopses
+
+print(tfidf_matrix.shape)
+
+terms = tfidf_vectorizer.get_feature_names()
+print terms
+
+
+# def main():
+# 	pass
+
+
+
+# if __name__ == '__main__':
+# 	main()
+
+
